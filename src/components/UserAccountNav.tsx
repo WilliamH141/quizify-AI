8d882ac20840fab54e5bb1f18c0657269d23c88a
William Huang <46873752+WilliamH141@users.noreply.github.com>
2025-10-21 23:53:07 +1300

    add dropdown menu to user accout nav

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { User } from "next-auth"
+import React from 'react'
+import { DropdownMenu, DropdownMenuItem } from "./ui/dropdown-menu"
+import { DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import Link from "next/link"
+import { signOut } from "next-auth/react"
+import { Button } from "./ui/button"
+
+type Props = {
+    user: Pick<User, "name" | "image" | "email">
+}
+
+const UserAccountNav = ({user}: Props) => {
+  return (
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            {/* user avatar */}
+            <Button>Hi</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className = "bg-white" align ="end">
+            <div className = "flex items-center justify-start gap-2 p-2">
+                <div className = "flex flex-col space-y-1 leading-none">
+                    {user.name && <p className = "font-medium" > {user.name} </p>}
+                    {
+                        user.email && (
+                            <p className = "w-[200px] truncate text-sm text-zinc-700">
+                                {user.email}
+                            </p>
+                        )
+                    }
+                </div>
+            </div>
+
+            <DropdownMenuSeparator/>
+            <DropdownMenuItem asChild>
+                <Link href = '/'>Meow</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator/>
+
+            <DropdownMenuItem onClick={(e) => {
+                e.preventDefault
+                signOut().catch(console.error)
+            }}
+            className = "text-red-600 cursor-pointer">
+                Sign Out
+            </DropdownMenuItem>
+
+
+
+
+        </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserAccountNav
