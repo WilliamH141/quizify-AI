f29b4731c82339285508efa8f1e14ba575e4e089
William Huang <46873752+WilliamH141@users.noreply.github.com>
2025-10-22 21:56:16 +1300

    feat: add basic home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+import SignInButton from "@/components/SignInButton";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { prisma } from "@/lib/db";
-export default function Home() {
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {  
+  const session = await getAuthSession()
+
+  if (session?.user) {
+    // This means the user is logged in
+    return redirect("/dashboard")
+  }
   
   return (
-    <Button>Hello World</Button>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card className = "w-[300px]">
+        <CardHeader>
+          <CardTitle>
+            Welcome to Quizify!
+          </CardTitle>
+          <CardDescription>
+            Quizify is a quiz app that allows you to create and share quizzes with your friends.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text = "Sign in with Google!"/> 
+        </CardContent>
+      </Card>
+    </div>
+
+
   );
 }
