67d32b4462781e19622061386855ff4b22a82111
William Huang <46873752+WilliamH141@users.noreply.github.com>
2025-11-14 23:56:32 +1300

    feat(api): add quiz creation POST route

---
+++ src/app/api/auth/[...nextauth]/questions/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import { ZodError } from "zod";
+import { strict_output } from "@/lib/gpt";
+
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { amount, topic, type } = quizCreationSchema.parse(body);
+
+    let questions: any;
+
+    if (type === "mcq") {
+      questions = await strict_output(
+        "You are a helpful ai that is able to generate a pair of questions and answers. the length of the answer should never exceed 15 words. store all the pairs of answers an questions in a JSON array",
+        `You are to generate a random hard open-ended question about ${topic}`,
+        {}
+      );
+    }
+
+    return NextResponse.json({
+      hello: "world",
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        { error: error.issues },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
