3fc96fe94e533a883c929b30f756c431e0ff63d8
William Huang <46873752+WilliamH141@users.noreply.github.com>
2025-12-02 23:57:15 +1300

    feat: wrap app with QueryClientProvider for react query support

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,13 +3,16 @@
 import { SessionProvider } from 'next-auth/react';
 import React from 'react'
 import { ThemeProvider as NextThemesProvider, ThemeProvider } from "next-themes"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
+const queryClient = new QueryClient()
 
 const Providers = ({ children, ...props }: ThemeProviderProps) => {
   return (
+    <QueryClientProvider client = {queryClient}>
     <ThemeProvider attribute = "class" defaultTheme='system' enableSystem>
 
       <SessionProvider>
@@ -17,7 +20,9 @@ const Providers = ({ children, ...props }: ThemeProviderProps) => {
       </SessionProvider>
 
 
-    </ThemeProvider>
+    </ThemeProvider>      
+    </QueryClientProvider>
+
     
   )
 }
