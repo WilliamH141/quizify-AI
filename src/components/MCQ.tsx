682a384afe42f86ad1045651ea69b6fa8e55fca9
William Huang <46873752+WilliamH141@users.noreply.github.com>
2025-12-12 23:59:23 +1300

    feat: show topic on mcq page

--- src/components/MCQ.tsx
+++ src/components/MCQ.tsx
@@ -5,9 +5,21 @@ type Props = {
     game: Game & {questions: Pick<Question, "id" | "options" | "question">[]}
 }
 
-const MCQ = (props: Props) => {
+const MCQ = ({game}: Props) => {
   return (
-    <div>MCQ</div>
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 md:w-[80vw] max-w-4xl w-[90vw] top-1/2 left-1/2">
+        <div className="flex flex-row justify-between" >
+            {/* topic */}
+
+            <p>
+                <span className = "text-slate-400">Topic</span>
+                <span className = "px-2 py-1 text-white rounded-lg bg-slate-800">{game.topic}</span>
+            </p>
+
+        </div>
+
+    </div>
+
   )
 }
 
