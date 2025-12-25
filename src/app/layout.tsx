aef9f2f85e75f3c72a9750d740538bdae99a34d2
William Huang <46873752+WilliamH141@users.noreply.github.com>
2025-12-25 23:57:46 +1300

    feat: add sonner toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -26,6 +27,7 @@ export default function RootLayout({
 
               <Navbar/>
               {children}
+              <Toaster/>
 
             </Providers>
 
